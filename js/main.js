4d41ded76bc6ef14fd8a597b2180ba9bdd0ff20d
crickshaw <crickshaw78@gmail.com>
2026-04-27 11:11:55 +0200

    fix: fix the issue that shows space instead previous projects on mobile screen

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@
             ro.unobserve(entry.target);
           }
         });
-      }, { rootMargin: '0px 0px -10% 0px', threshold: 0.08 });
+      }, { rootMargin: '0px 0px -10% 0px', threshold: 0 });
       revealTargets.forEach(el => ro.observe(el));
     } else {
       revealTargets.forEach(el => el.classList.add('is-in'));
